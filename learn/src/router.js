461ea50d7f91373598d45bd0554ccea5f5a629f5
liuhaokun <2568601148@qq.com>
2019-09-04 19:58:17 +0800

    lhc

---
+++ learn/src/router.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      redirect:"/music"
+    },
+    {
+      path: '*',
+      name: '404',
+      component: () => import('./views/404/')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('./views/login/')
+    },
+    {
+      path: '/music',
+      name: 'music',
+      component: () => import('./views/music/')
+    },
+    {
+      path: '/registry',
+      name: 'registry',
+      component: () => import('./views/registry/')
+    }
+  ]
+})
